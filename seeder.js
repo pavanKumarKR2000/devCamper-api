4955ee3fc2556bd40636bf2bd28042606c5ef777
Pavankumar K R <pavan123456pop@gmail.com>
2024-12-13 20:39:18 +0530

    add seerder.js file

---
+++ seeder.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+/** load env vars */
+dotenv.config({ path: "./config/config.env" });
+
+/** load models */
+const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
+const User=require("./models/User");
+const Review=require("./models/Review");
+
+/** connect to DB */
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+/** read json files */
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8"),
+);
+
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8"),
+);
+
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8"),
+);
+
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8"),
+);
+
+/** import into DB */
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+    await User.create(users);
+    await Review.create(reviews);
+    console.log("data imported...");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/** delete from DB */
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log("data deleted...");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
