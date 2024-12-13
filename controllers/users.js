23d264b26aa7c58f4ee12d4d01e906f6c029de08
Pavankumar K R <pavan123456pop@gmail.com>
2024-12-13 20:37:26 +0530

    add controllers folder

---
+++ controllers/users.js
@@ -0,0 +1,59 @@
+const asyncHandler = require("../middlewares/async");
+const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
+
+// @desc Get all users
+// @route POST /api/v1/auth/users
+// @access Private/Admin
+const getUsers = asyncHandler(async (req, res, next) => {
+    res.status(200).json(res.advancedResults);
+});
+
+// @desc Get single user
+// @route GET /api/v1/auth/users/:id
+// @access Private/Admin
+const getUser = asyncHandler(async (req, res, next) => {
+    const user=await User.findById(req.params.id);
+    res.status(200).json({
+        success:true,
+        data:user
+    });
+});
+
+// @desc Create user
+// @route POST /api/v1/auth/users
+// @access Private/Admin
+const createUser = asyncHandler(async (req, res, next) => {
+    const user=await User.create(req.params.id);
+    res.status(201).json({
+        success:true,
+        data:user
+    });
+});
+
+// @desc Update user
+// @route PUT /api/v1/auth/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res, next) => {
+    const user=await User.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true
+    });
+    res.status(200).json({
+        success:true,
+        data:user
+    });
+});
+
+// @desc Delete user
+// @route DELETE /api/v1/auth/users/:id
+// @access Private/Admin
+const deleteUser = asyncHandler(async (req, res, next) => {
+    await User.findByIdAndUpdate(req.params.id);
+    res.status(200).json({
+        success:true,
+        data:{}
+    });
+});
+
+module.exports={getUsers,getUser,createUser,updateUser,deleteUser};
